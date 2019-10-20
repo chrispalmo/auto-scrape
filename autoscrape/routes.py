796c993fafbef6cd33d290eb5a3a201662c641ed
alex.britten <alex.britten@superloop.com>
2019-10-20 16:35:38 +1000

    Added basic scraper output in browser

--- autoscrape/routes.py
+++ autoscrape/routes.py
@@ -10,5 +10,5 @@ def home():
 @app.route("/test_scrape")
 def test_scrape():
     agent_os = request.user_agent.platform
-    scraper.test_scrape(agent_os)
-    return render_template('test_scrape.html', agent_os=agent_os)
+    scraper_output = scraper.test_scrape(agent_os)
+    return render_template('test_scrape.html', agent_os=agent_os, scraper_output=scraper_output)
