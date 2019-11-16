d1086c01a1161abee5eff5fb65981cc5c3651266
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2019-11-16 22:17:12 +1000

    Update scraper.py

--- autoscrape/scraper.py
+++ autoscrape/scraper.py
@@ -12,21 +12,38 @@ class Scraper():
             self.driver = webdriver.Chrome('./autoscrape/chromedriver.exe')
         else:
             self.driver = webdriver.Chrome('./autoscrape/chromedriver')
-
+        #max wait for page to load
+        self.driver.set_page_load_timeout(30)
+        #max wait for page elements to load
+        self.driver.implicitly_wait(30)
         self.session_id = session_id
         self.sessions = sessions
 
-    def test_scrape(self, scrape_url, filter_query1):
-        print(scrape_url)
-        self.driver.get(scrape_url)
+    def test_scrape(self, url, filter_query1):
+        print(url)
+        self.driver.get(url)
         time.sleep(1)
         try:
             print("scraping...")
-            posts = self.driver.find_elements_by_class_name(filter_query1)
-            return [post.text for post in posts]
+            elements = self.driver.find_elements_by_class_name(filter_query1)
+            return [element.text for element in elements]
 
         except Exception as e:
             return e
+
+    def get_url(self, url):
+        try:
+            self.log("get_url", url)
+            self.driver.get(url)
+        except Exception as e:
+            self.log(e)
+
+    def find_elements_by_class_name(self, query):
+        try:
+            self.log("find_elements_by_class_name",query)
+            self.driver.find_elements_by_class_name(url)
+        except Exception as e:
+            self.log(e)
 
     def destroy(self):
         self.driver.quit()
