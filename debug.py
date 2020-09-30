75219fec880433cfcf890be33fe8241d09e075de
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2020-09-30 23:21:36 +1000

    simplify selenium debugger

--- debug.py
+++ debug.py
@@ -8,8 +8,4 @@ How to use:
 3. driver.get(url)
 '''
 
-agent_os = input("Are you using Windows operating system? [Y/N] Default: [N] >>>")
-if (agent_os == 'y') or (agent_os) == 'Y':
-	driver = webdriver.Chrome('./autoscrape/chromedriver.exe')
-else:
-	driver = webdriver.Chrome('./autoscrape/chromedriver')
+driver = webdriver.Chrome('./autoscrape/chromedriver')
